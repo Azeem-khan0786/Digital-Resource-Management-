a8a57fc2778f532536f957bbbbc5ac2718d576ce
khan <khan@zmq.in>
2024-08-06 16:42:14 +0530

    change in category file

--- application/views/asset/category.php
+++ application/views/asset/category.php
@@ -1,6 +1,6 @@
 <!DOCTYPE html>
 <html lang="en">
-
+<!-- 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  -->
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
